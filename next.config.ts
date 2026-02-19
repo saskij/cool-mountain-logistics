408ead7364ee5d5f4e402e6ec584db46e5bac52d
Alexander Ashurov <bstuashurov@gmail.com>
2026-02-19 12:12:46 -0700

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/cool-mountain-logistics" : "",
+  assetPrefix: isProd ? "/cool-mountain-logistics/" : "",
 };
 
 export default nextConfig;
